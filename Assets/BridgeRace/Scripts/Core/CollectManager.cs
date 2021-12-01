cae2ae816d8deeeb73c28961bb51c40a7bf6ca1e
Abdullah NUR <48687868+nurabdullah@users.noreply.github.com>
2021-12-01 11:22:47 +0300

    Feat: Pooling system added

--- Assets/BridgeRace/Scripts/Core/CollectManager.cs
+++ Assets/BridgeRace/Scripts/Core/CollectManager.cs
@@ -1,10 +1,33 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollectManager : MonoBehaviour
+namespace BridgeRace.Scripts.Core
 {
-   
+    public class CollectManager : Singleton<CollectManager>
+    {
+        public List<Vector3> virtualPoints;
 
-    
+        private void Awake()
+        {
+            virtualPoints = new List<Vector3>();
+            for (float i = 0f; i < 10.1f; i += 2.5f)
+            {
+                for (float j = -5.0f; j < 5.1; j += 2.5f)
+                {
+                    var v3 = new Vector3(j, 0.1f, i);
+                    virtualPoints.Add(v3);
+                }
+            }
+            
+            for (float i = 40f; i < 50.1f; i += 2.5f)
+            {
+                for (float j = -5.0f; j < 5.1; j += 2.5f)
+                {
+                    var v3 = new Vector3(j, 9.1f, i);
+                    virtualPoints.Add(v3);
+                }
+            }
+        }
+    }
 }
--- Assets/BridgeRace/Scripts/Core/PoolManager.cs
+++ Assets/BridgeRace/Scripts/Core/PoolManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using BridgeRace.Scripts.Core;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace BridgeRace.Scripts
 {
@@ -11,33 +13,51 @@ namespace BridgeRace.Scripts
         private void Awake()
         {
             if (Instance == null) Instance = this;
-            else Destroy(Instance);
+            else Destroy(Instance); 
         }
 
 
         private void Start()
         {
+            Pool1 = new Queue<GameObject>();
+            Pool2 = new Queue<GameObject>();
+            Pool3 = new Queue<GameObject>();
+            
             FillPool(Pool1GameObject[0],Pool1Parent[0],Pool1,Pool1Count);
-            FillPool(Pool1GameObject[1],Pool1Parent[1],Pool1,Pool1Count);
-            FillPool(Pool1GameObject[2],Pool1Parent[2],Pool1,Pool1Count);
+            FillPool(Pool1GameObject[1],Pool1Parent[1],Pool2,Pool1Count);
+            FillPool(Pool1GameObject[2],Pool1Parent[2],Pool3,Pool1Count);
+
+            queues = new List<Queue<GameObject>>();
+            queues.Add(Pool1);
+            queues.Add(Pool2);
+            queues.Add(Pool3);
+            
+            // Virtual Pointlere ulas ve positionlarına collectableları ata
+            for (int i = 0; i < CollectManager.Instance.virtualPoints.Count; i++)
+            {
+                var arrayElement = Random.Range(0, queues.Count);
+                var collect = this.DequeuePoolObject(queues[arrayElement]);
+                collect.transform.position = CollectManager.Instance.virtualPoints[i];
+            }
         }
+
+        public List<Queue<GameObject>> queues;
 
         #region Pools
         [Space(10)]
         public GameObject[] Pool1GameObject;
         public Transform[]  Pool1Parent;
         public int Pool1Count;
-        [HideInInspector] public Queue<GameObject> Pool1 = new Queue<GameObject>();
+        [HideInInspector] public Queue<GameObject> Pool1;
+        [HideInInspector] public Queue<GameObject> Pool2;
+        [HideInInspector] public Queue<GameObject> Pool3;
         #endregion
 
         private void FillPool(GameObject prefab, Transform poolParent, Queue<GameObject> pool, int poolCount)
         {
-            pool = new Queue<GameObject>();
-            GameObject poolObject;
-
             for (int i = 0; i < poolCount; i++)
             {
-                poolObject = Instantiate(prefab, poolParent);
+                var poolObject = Instantiate(prefab, poolParent);
                 poolObject.SetActive(false);
                 pool.Enqueue(poolObject);
             }
